2df9006b303e6799f588f37e3279b25053ab67fa
Mr. Joe <radics.pepe@gmail.com>
2022-06-23 12:45:16 +0200

    save_2_06.23

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 //Lazy User function
 
-
 let Xbutton = document.getElementById("closebutton");
 let go = document.getElementById("goButton");
-
 
 if (location == 0) {
   console.log("Jóó!");
@@ -17,46 +15,45 @@ function close() {
 
 //-------------------------------------------------------------------------------------
 
-// Licence function 
+// Licence function
 
 let XLicensButton = document.getElementById("closebuttonLicens");
 let infoButton = document.getElementById("js-ifnotButton");
 
-
 infoButton.addEventListener("click", licensOpen);
 
 function licensOpen() {
-  console.log('Super');
+  console.log("Super");
   document.querySelector(".licence").style.display = "flex";
 }
 
-
-
 XLicensButton.addEventListener("click", licensClose);
 
-function licensClose(){
+function licensClose() {
   document.querySelector(".licence").style.display = "none";
-
 }
-
-
-
-
-
-
 
 //--------------------------------------------------------------------------------------
 
-//Az első generálás 
+//Az első generálás
 
-go.addEventListener("click", first );
+go.addEventListener("click", first);
 
-function first (){
-let location = document.getElementById("location").selectedIndex;
-    console.log(location)
+function first() {
+  let location = document.getElementById("location").selectedIndex;
 
-     if (location == 0) {
-        document.querySelector(".lazyUser").style.display = "flex";
-        console.log('Menny aludni!');
-      }  
+  if (location === 0) {
+    document.querySelector(".lazyUser").style.display = "flex";
+    return;
+  }
+  if (location > 0)
+    document.querySelector(".headerLogo").style.display = "none";
+  document.querySelector("#MonsterCalc").style.display = "none";
+  document.querySelector("#goButton").style.display = "none";
+  document.querySelector(".footerText").style.display = "none";
+  document.querySelector(".footerIcon").style.display = "none";
+  document.querySelector(".creatingImg").style.display = "block";
+  document.querySelector(".creatingStat").style.display = "block";
+  document.querySelector("#MonsterCalc").style.display = "block";
+  document.querySelector(".againButton").style.display = "flex";
 }
